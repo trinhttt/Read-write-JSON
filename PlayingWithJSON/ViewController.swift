bd58e0e457f2256b85df1073ac0a791652217a88
Trinh Thai <32394032+trinhttt@users.noreply.github.com>
2020-09-15 14:52:00 +0700

    *Read json with Decodable

--- PlayingWithJSON/ViewController.swift
+++ PlayingWithJSON/ViewController.swift
@@ -12,11 +12,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        readJSON()
+//        readJSON_JSONSerialization()
+        readJSON_Coable()
     }
 
-    func readJSON() {
+    func readJSON_JSONSerialization() {
         if let url = Bundle.main.url(forResource: "Test", withExtension: "json") {
             do {
                 let data = try Data(contentsOf: url)
@@ -26,44 +26,58 @@ class ViewController: UIViewController {
                     print(data)
                 }
             } catch {
-                // handle error
+                print(error.localizedDescription)
             }
         }
     }
     
     func readJSON_Coable() {
-        
+        if let url = Bundle.main.url(forResource: "Test", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let response = try JSONDecoder().decode(APIBase<StudentResponse>.self, from: data)
+                print(response.data[0])
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+struct APIError: Decodable, CustomDebugStringConvertible {
+    let code: String
+    let message: String
+
+    var debugDescription: String {
+        return "code=\(code), message=\(message)"
     }
 }
 
-//struct UploadImageResponse: Decodable {
-//    let name: String
-//    let meta: MetaData
-//    let tookAt: String?
-//    let originalFileName: String
-//    let imageId: Int
-//
-//    private enum CodingKeys: String, CodingKey {
-//        case name
-//        case meta
-//        case tookAt = "took_at"
-//        case originalFileName = "original_file_name"
-//        case imageId = "image_id"
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        name = try container.decode(String.self, forKey: .name)
-//        meta = try container.decode(MetaData.self, forKey: .meta)
-//        tookAt = try container.decodeIfPresent(String.self, forKey: .tookAt)
-//        originalFileName = try container.decode(String.self, forKey: .originalFileName)
-//        imageId = try container.decode(Int.self, forKey: .imageId)
-//    }
-//}
-//
-//struct MetaData: Decodable {
-//    let mime: String
-//    let rotate: Int
-//    let width: Int
-//    let height: Int
-//}
+struct APIBase<T: Decodable>: Decodable {
+    let data: [T]
+    let status: Int
+    let errors: [APIError]
+}
+
+struct StudentResponse: Decodable {
+    let info: PersonalInfo
+    let avatar: String?
+    let studentId: String
+
+    private enum CodingKeys: String, CodingKey {
+        case info
+        case avatar
+        case studentId = "student_id"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        info = try container.decode(PersonalInfo.self, forKey: .info)
+        avatar = try container.decodeIfPresent(String.self, forKey: .avatar)
+        studentId = try container.decode(String.self, forKey: .studentId)
+    }
+}
+
+struct PersonalInfo: Decodable {
+    let name: String
+    let birthday: String
+}
